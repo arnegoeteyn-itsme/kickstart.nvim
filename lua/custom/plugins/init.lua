33ba3bce7d45fa6bded86e60a984c87bba3f6458
Arne Goeteyn <arnegoeteyn@itsme-id.com>
2025-07-30 10:00:29 +0200

    add session support

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -7,4 +7,16 @@ return {
     'sindrets/diffview.nvim',
   },
   { 'kevinhwang91/nvim-ufo', dependencies = 'kevinhwang91/promise-async' },
+  {
+    'rmagatti/auto-session',
+    lazy = false,
+
+    ---enables autocomplete for opts
+    ---@module "auto-session"
+    ---@type AutoSession.Config
+    opts = {
+      suppressed_dirs = { '~/', '~/Projects', '~/Downloads', '/' },
+      -- log_level = 'debug',
+    },
+  },
 }
