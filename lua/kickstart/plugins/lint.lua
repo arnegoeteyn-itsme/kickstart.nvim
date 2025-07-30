ebf7deb38c867fe85f5d3d67cf3bbe185a03b658
Arne Goeteyn <arnegoeteyn@itsme-id.com>
2025-07-30 09:49:16 +0200

    add snippets and golangcilint

--- lua/kickstart/plugins/lint.lua
+++ lua/kickstart/plugins/lint.lua
@@ -40,6 +40,7 @@ return {
       -- lint.linters_by_ft['ruby'] = nil
       -- lint.linters_by_ft['terraform'] = nil
       -- lint.linters_by_ft['text'] = nil
+      lint.linters_by_ft['go'] = { 'golangcilint' }
 
       -- Create autocommand which carries out the actual linting
       -- on the specified events.
