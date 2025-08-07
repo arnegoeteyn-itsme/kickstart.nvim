96b1a31b7b1eb48bdb976ac230894a92669d494c
Arne Goeteyn <arne.goeteyn@itsme-id.com>
2025-08-07 14:35:14 +0200

    move some plugins to seperate files

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -6,9 +6,9 @@ return {
   {
     'sindrets/diffview.nvim',
   },
-  { 'kevinhwang91/nvim-ufo', dependencies = 'kevinhwang91/promise-async' },
   {
     'rmagatti/auto-session',
+    enabled = false,
     lazy = false,
 
     ---enables autocomplete for opts
@@ -18,61 +18,5 @@ return {
       suppressed_dirs = { '~/', '~/Projects', '~/Downloads', '/' },
       -- log_level = 'debug',
     },
-  },
-  -- {
-  --   'rest-nvim/rest.nvim',
-  --   dependencies = {
-  --     'nvim-treesitter/nvim-treesitter',
-  --     opts = function(_, opts)
-  --       opts.ensure_installed = opts.ensure_installed or {}
-  --       table.insert(opts.ensure_installed, 'http')
-  --     end,
-  --   },
-  -- },
-  {
-    'diepm/vim-rest-console',
-    ft = 'http',
-    init = function()
-      -- Filetype detection
-
-      vim.api.nvim_create_autocmd({ 'BufNewFile', 'BufRead', 'BufReadPost' }, {
-
-        pattern = '*.http',
-
-        callback = function()
-          vim.bo.syntax = 'rest'
-
-          vim.bo.filetype = 'rest'
-        end,
-      })
-
-      -- Global settings
-
-      vim.g.vrc_trigger = '<leader>vrc'
-
-      vim.g.vrc_curl_opts = {
-
-        ['-L'] = '',
-
-        ['-i'] = '',
-
-        ['-s'] = '',
-
-        ['-k'] = '',
-      }
-    end,
-
-    config = function()
-      -- Buffer-local setting: set default response content type
-
-      vim.api.nvim_create_autocmd('FileType', {
-
-        pattern = 'rest',
-
-        callback = function()
-          vim.b.vrc_response_default_content_type = 'application/json'
-        end,
-      })
-    end,
   },
 }
